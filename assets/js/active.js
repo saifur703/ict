9eac7e0c699ce578c52380fba8e10e9fcf839c4d
Saifur Rahman <saifur703@gmail.com>
2018-05-08 00:51:38 +0600

    Add files via upload

---
+++ assets/js/active.js
@@ -0,0 +1,17 @@
+(function($){
+	"use strict";
+	
+	jQuery(document).ready(function($){
+        $(".element").typed({
+			strings: ["RECOMMENDED", "RECOMMENDED"],
+			typeSpeed: 100,
+            loop: true
+		});
+	});
+	
+	
+	jQuery(window).on("load", function(){
+		
+	});
+	
+})(jQuery);
